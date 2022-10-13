22b61eceb00ae7a0d9b8c0a203c8ee62234d3d24
Nguyen Danh Binh Giang <NguyenDanhBinhGiang@gmail.com>
2022-10-13 09:44:57 +0700

    create 2 new groups

--- purchase_inherit/__manifest__.py
+++ purchase_inherit/__manifest__.py
@@ -1,35 +1,13 @@
 # -*- coding: utf-8 -*-
 {
     'name': "purchase_inherit",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
+    'category': 'Administration',
+    'depends': ['base', 'hr', 'purchase'],
     'data': [
         # 'security/ir.model.access.csv',
+        'security/group.xml',
+        'data/data.xml',
         'views/views.xml',
         'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
     ],
 }
