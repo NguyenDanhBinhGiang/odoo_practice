11232c239fcb83500d548d444f91a15fe00e91bc
Nguyen Danh Binh Giang <NguyenDanhBinhGiang@gmail.com>
2022-10-17 11:05:33 +0700

    created and give permission to the initial 3 groups

---
+++ crm_sale_inherit/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': "crm_sale_inherit",
+    'category': 'Administration',
+    'depends': ['base', 'crm', 'sale_management'],
+    'data': [
+        'security/group.xml',
+        'security/security_rules.xml',
+    ],
+}
---
+++ crm_sale_inherit/models/res_users_inherit.py
@@ -0,0 +1,5 @@
+from odoo import models, fields, api
+
+
+class ResUsersInherit(models.Model):
+    _inherit = 'res.users'
